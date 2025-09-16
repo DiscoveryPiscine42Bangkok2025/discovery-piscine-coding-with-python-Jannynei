53c59c5759157958b686ebc059df061af3285b7d
Jannynei <66050921@kmitl.ac.th>
2025-09-17 01:48:59 +0700

    well

--- cell04/ex05/up_low.py
+++ cell04/ex05/up_low.py
@@ -1,11 +1,2 @@
 #!/usr/bin/env python
-def main():
-    msg = input("")
-    for char in msg:
-        if char.islower():
-            print(char.upper(), end="")
-        elif char.isupper():
-            print(char.lower(), end="")
-        else:
-            print(char, end="")
-main()
+print(input().swapcase())
