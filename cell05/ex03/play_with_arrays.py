ea36c0349ac16378da39186b98f2a31792c84fd5
Jannynei <66050921@kmitl.ac.th>
2025-09-17 03:28:14 +0700

    sys

--- cell05/ex03/play_with_arrays.py
+++ cell05/ex03/play_with_arrays.py
@@ -1,4 +1,5 @@
 #!/usr/bin/env python
 nums = [2, 8, 9, 48, 8, 22, -12, 2]
+result = {n+2 for n in nums if n >= 8}
 print(nums)
-print([n+2 for n in nums if n>=8])
+print(result)
---
+++ cell05/ex05/aff_first_param.py
@@ -0,0 +1,3 @@
+#!/usr/bin/env python
+import sys
+print(sys.argv[1] if len(sys.argv) > 1 else "none")
